52d7d620c41f7e4a7fd006c98e3b95b196441ef5
julinhafmm <juliafernanda.m.moraes@gmail.com>
2025-08-18 20:41:40 -0300

    Commit aula de  api

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const app = require()
+const port = 3000 
+
+app.use(express.json())
+
+app.listen(port, () => {
+    console.log("Servidor De API Funcionando")
+})
