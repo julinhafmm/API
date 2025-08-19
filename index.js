e1e3163a708d8b0cf32a2d9d68c951c0261fe5f8
julinhafmm <juliafernanda.m.moraes@gmail.com>
2025-08-18 21:17:07 -0300

    Primeiro commit

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require("express")
-const app = require()
+const app = express()
 const port = 3000 
 
 app.use(express.json())
+
+let alunos = [
+    {id: 1 , nome: "Julia"}
+]
+
+app.get('/aluno', (req, res)=> {
+    res.json(alunos)
+})
+
+
+app.post('/aluno', (req, res) =>{
+    const novoAluno = { id: alunos.length + 1, ...req.body}
+    alunos.push(novoAluno)
+    res.status(201).json(novoAluno)
+})
+
 
 app.listen(port, () => {
     console.log("Servidor De API Funcionando")
